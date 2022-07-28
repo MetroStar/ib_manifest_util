d395cf80ebcbec5d07176a318ea24bf707d8e7a4
Eric Kelly <ericdatakelly@gmail.com>
2022-07-28 17:32:14 -0600

    Format with black via pre-commit

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages, find_namespace_packages
 from pathlib import Path
+
+from setuptools import find_namespace_packages, find_packages, setup
 
 root_dir = Path(__file__).absolute().parent
 
@@ -11,5 +12,7 @@ setup(
     version=__version__,
     install_requires=["click", "ruamel.yaml", "conda_vendor"],
     packages=find_packages(),
-    entry_points={"console_scripts": ["ib_manifest_util = ib_manifest_util.__main__:main"]}
+    entry_points={
+        "console_scripts": ["ib_manifest_util = ib_manifest_util.__main__:main"]
+    },
 )
