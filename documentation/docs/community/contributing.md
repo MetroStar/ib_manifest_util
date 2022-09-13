1aa31111f61a1f0ed248e88eddeecde4bd7c16f3
Kim Pevey <kcpevey@gmail.com>
2022-09-13 11:12:42 -0500

    add docs

---
+++ documentation/docs/community/contributing.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 4
+sidebar_label: Contributing
+---
+# Contributing to the Iron Bank Manifest Utility
+
+Welcome 👋🏼!
+
+Thanks for being interested in contributing to the Iron Bank Manifest Utility (IB Manifest). We’re glad you want to join this community!
+The IB Manifest team truly believes that everyone belongs in open source, and we are dedicated to making you feel welcome.
+
+All contributions are welcome, including issues, contributing code, new docs as well as updates and tweaks, blog posts, helping out people, organizing community events, working on accessibility and design items, and more. By contributing to open source projects you can connect with people, learn new skills, become a subject-matter expert, and apply all learnings to your own projects.
---
+++ documentation/src/components/HomepageFeatures/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch,
+  } from 'react-router-dom';
+
+
+const FeatureList = [
+  {
+    title: <Link to="/docs/getting-started/installation">Installation</Link>,
+    url: "/docs/getting-started/installation",
+    Svg: require('@site/static/img/construction.svg').default,
+    imageLink: "/docs/getting-started/installation",
+    description: (
+      <>
+        Getting started using the IB Manifest Util is straightforward and takes
+        less than 5 minutes to get set up.
+      </>
+    ),
+  },
+  {
+    title: <Link to="/docs/getting-started/installation">Update your IB Container</Link>,
+    Svg: require('@site/static/img/update.svg').default,
+    description: (
+      <>
+        Need to add a package? IB Manifest Util provides a single high-level command
+        for generating all the files you'll need to update your container repo!
+      </>
+    ),
+  },
+  {
+    title: <Link to="/docs/community/contributing">Community</Link>,
+    Svg: require('@site/static/img/contribute.svg').default,
+    description: (
+      <>
+        We welcome contributions and questions from the community :)
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
