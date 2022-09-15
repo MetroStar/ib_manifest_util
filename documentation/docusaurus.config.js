5b1d066658189c0d51731a208c513c6926c5f7b6
Kim Pevey <kcpevey@gmail.com>
2022-09-15 13:07:06 -0500

    fix broken links

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -88,8 +88,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Getting Started',
+                to: 'getting-started/installation',
               },
             ],
           },
