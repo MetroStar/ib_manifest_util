8a9f2677194535c2626113b1e341529fe7d9c9da
Kim Pevey <kcpevey@gmail.com>
2022-10-17 10:31:52 -0500

    linting

--- examples/example_update_repo.py
+++ examples/example_update_repo.py
@@ -10,7 +10,7 @@ update_repo(
     repo_dir=repo_dir,
     dockerfile_version="9999",
     # local_env_path=repo_dir.joinpath("scripts", "local_channel_env.yaml"),
-    startup_scripts_path='None',
+    startup_scripts_path="None",
     # output_hardening_path=output_dir.joinpath("output_hardening_manifest.yaml"),
     # output_dockerfile_path=output_dir.joinpath("output_dockerfile"),
     # dockerfile_template_path=None,
