ca06c3c608113d64eef93a0d2bf2d4021d34d0a1
Kim Pevey <kcpevey@gmail.com>
2022-10-18 13:44:13 -0500

    linting

--- examples/example_update_repo.py
+++ examples/example_update_repo.py
@@ -9,8 +9,14 @@ output_dir = Path(".").joinpath("example_output")
 update_repo(
     repo_dir=repo_dir,  # path to the IB Repo
     dockerfile_version="9999",  # incremental IB Repo version for dockerfile
-    local_env_path=repo_dir.joinpath("scripts", "local_channel_env.yaml"),  # defaults to the one in `repo_dir`
-    output_hardening_path=output_dir.joinpath("output_hardening_manifest.yaml"),  # defaults to overwrite the one in `repo_dir`
-    output_dockerfile_path=output_dir.joinpath("output_dockerfile"), # defaults to overwrite the one in `repo_dir`
+    local_env_path=repo_dir.joinpath(
+        "scripts", "local_channel_env.yaml"
+    ),  # defaults to the one in `repo_dir`
+    output_hardening_path=output_dir.joinpath(
+        "output_hardening_manifest.yaml"
+    ),  # defaults to overwrite the one in `repo_dir`
+    output_dockerfile_path=output_dir.joinpath(
+        "output_dockerfile"
+    ),  # defaults to overwrite the one in `repo_dir`
     dockerfile_template_path=None,  # defaults to `repo_dir/scripts/Dockerfile.tpl`
 )
